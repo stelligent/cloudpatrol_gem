1355c5cd06ad85a2d8dadf1da1175c2a7d00e6a4
Alexander <skiba-alex@ya.ru>
2013-07-06 19:11:56 +0700

    Return details of every delete for logging, temporarily remove pending tasks

--- lib/cloudpatrol/task.rb
+++ lib/cloudpatrol/task.rb
@@ -7,10 +7,10 @@ module Cloudpatrol::Task
     end
 
     def clean_users
-      deleted = 0
+      deleted = []
       @gate.users.each do |user|
-        unless user.name =~ /_/ or user.mfa_devices.count > 0
-          deleted += 1
+        unless user.name =~ /^_/ or user.mfa_devices.count > 0
+          deleted << user
           user.delete!
         end
       end
@@ -24,7 +24,7 @@ module Cloudpatrol::Task
     end
 
     def clean_security_groups
-      deleted = 0
+      deleted = []
       protected_groups = []
       @gate.security_groups.each do |sg|
         sg.ip_permissions.each do |perm|
@@ -35,7 +35,7 @@ module Cloudpatrol::Task
       end
       @gate.security_groups.each do |sg|
         if sg.exists? and !protected_groups.include?(sg) and sg.instances.count == 0
-          deleted += 1
+          deleted << sg
           sg.delete
         end
       end
@@ -49,11 +49,11 @@ module Cloudpatrol::Task
     end
 
     def clean_apps allowed_age
-      deleted = 0
+      deleted = []
       @gate.describe_stacks[:stacks].each do |stack|
         @gate.describe_apps(stack_id: stack[:stack_id])[:apps].each do |app|
           if (Time.now - Time.parse(app[:created_at])).to_i > allowed_age * 24 * 60 * 60
-            deleted += 1
+            deleted << app
             @gate.delete_app app_id: app[:app_id]
           end
         end
@@ -62,11 +62,11 @@ module Cloudpatrol::Task
     end
 
     def clean_instances allowed_age
-      deleted = 0
+      deleted = []
       @gate.describe_stacks[:stacks].each do |stack|
         @gate.describe_instances(stack_id: stack[:stack_id])[:instances].each do |instance|
           if (Time.now - Time.parse(instance[:created_at])).to_i > allowed_age * 24 * 60 * 60
-            deleted += 1
+            deleted << instance
             @gate.delete_instance instance_id: instance[:instance_id]
           end
         end
@@ -75,11 +75,11 @@ module Cloudpatrol::Task
     end
 
     def clean_layers allowed_age
-      deleted = 0
+      deleted = []
       @gate.describe_stacks[:stacks].each do |stack|
         @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
           if (Time.now - Time.parse(layer[:created_at])).to_i > allowed_age * 24 * 60 * 60
-            deleted += 1
+            deleted << layer
             @gate.delete_layer layer_id: layer[:layer_id]
           end
         end
@@ -88,10 +88,10 @@ module Cloudpatrol::Task
     end
 
     def clean_stacks allowed_age
-      deleted = 0
+      deleted = []
       @gate.describe_stacks[:stacks].each do |stack|
         if (Time.now - Time.parse(stack[:created_at])).to_i > allowed_age * 24 * 60 * 60
-          deleted += 1
+          deleted << stack
           @gate.delete_stack stack_id: stack[:stack_id]
         end
       end
@@ -105,10 +105,10 @@ module Cloudpatrol::Task
     end
 
     def clean_stacks allowed_age
-      deleted = 0
+      deleted = []
       @gate.stacks.each do |stack|
         if (Time.now - stack.creation_time).to_i > allowed_age * 24 * 60 * 60
-          deleted += 1
+          deleted << stack
           stack.delete
         end
       end
