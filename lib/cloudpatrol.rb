89256f06e69f5b8a4152a9567803ad6bfaea46cd
Alexander <skiba-alex@ya.ru>
2013-07-14 23:48:57 +0700

    Change logging format a bit

--- lib/cloudpatrol.rb
+++ lib/cloudpatrol.rb
@@ -7,7 +7,7 @@ module Cloudpatrol
     response = {}
     table_name = "cloudpatrol-log" unless table_name
     (response[:task] = Task.const_get(klass).new(aws_credentials).send(method, *args)) rescue false
-    (response[:log] = Task::DynamoDB.new(aws_credentials).log(table_name, { klass: klass, method: method, args: args }, response[:task])) rescue false
+    (response[:log] = Task::DynamoDB.new(aws_credentials).log(table_name, { class: klass, method: method, args: args }, response[:task])) rescue false
     response
   end
 end
--- lib/cloudpatrol/task/dynamo_db.rb
+++ lib/cloudpatrol/task/dynamo_db.rb
@@ -11,12 +11,15 @@ module Cloudpatrol
       def log table_name, action, response
         return false unless table_name
         t = @gate.tables[table_name]
+
+        # Ensuring the table exists
         unless t.exists?
-          t = @gate.tables.create(table_name, 1, 1)
           puts "Creating DynamoDB table \"#{table_name}\", wait a while..."
+          t = @gate.tables.create(table_name, 1, 1)
           sleep 1 while t.status == :creating
           puts "Table created"
         end
+
         if t.status == :active
           i = t.items.create(id: SecureRandom.uuid, action: action.to_s, response: response.to_s, time: Time.now.to_s)
           i
