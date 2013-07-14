977e0930cd50d5b358f67920ad710e278bd3a2f4
Alexander <skiba-alex@ya.ru>
2013-07-14 23:46:58 +0700

    Several fixes

--- lib/cloudpatrol.rb
+++ lib/cloudpatrol.rb
@@ -5,9 +5,9 @@ require 'core_ext/integer.rb'
 module Cloudpatrol
   def self.perform aws_credentials, table_name, klass, method, *args
     response = {}
-    table_name = "cloudpatrol-log" if table_name.blank?
-    response[:task] = Task.const_get(klass).new(aws_credentials).send(method, *args)
-    response[:log] = Task::DynamoDB.new(aws_credentials).log(table_name, { klass: klass, method: method, args: args }, response[:task])
+    table_name = "cloudpatrol-log" unless table_name
+    (response[:task] = Task.const_get(klass).new(aws_credentials).send(method, *args)) rescue false
+    (response[:log] = Task::DynamoDB.new(aws_credentials).log(table_name, { klass: klass, method: method, args: args }, response[:task])) rescue false
     response
   end
 end
