b260d69ae93bdbe394d96299e11abac6b228b754
Alexander <skiba-alex@ya.ru>
2013-07-16 02:34:36 +0700

    Log AWS errors properly

--- lib/cloudpatrol.rb
+++ lib/cloudpatrol.rb
@@ -6,7 +6,13 @@ module Cloudpatrol
   def self.perform aws_credentials, table_name, klass, method, *args
     response = {}
     table_name = "cloudpatrol-log" unless table_name
-    (response[:task] = Task.const_get(klass).new(aws_credentials).send(method, *args)) rescue false
+
+    response[:task] = begin
+      Task.const_get(klass).new(aws_credentials).send(method, *args))
+    rescue AWS::Errors::Base => e
+      "Error: #{e}"
+    end
+
     (response[:log] = Task::DynamoDB.new(aws_credentials).log(table_name, { class: klass, method: method, args: args }, response[:task])) rescue false
     response
   end
