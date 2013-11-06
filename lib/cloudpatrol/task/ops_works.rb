716d738d37edba2db94feb81231a273854ee301f
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-11-05 22:43:15 -1000

    I'm not going to say the OpsWorks API is the worst I've ever worked with, but I'm probably okay saying that they all hung out in high school together. Finally got stacks deleting cleanly, but the tests are a mess right now.

--- lib/cloudpatrol/task/ops_works.rb
+++ lib/cloudpatrol/task/ops_works.rb
@@ -1,10 +1,12 @@
 require 'aws'
 
+
 module Cloudpatrol
   module Task
     class OpsWorks
       def initialize cred
         @gate = ::AWS::OpsWorks::Client.new(cred)
+        @sleeptime = 10
       end
 
       def clean_apps allowed_age
@@ -51,18 +53,46 @@ module Cloudpatrol
         @gate.describe_stacks[:stacks].each do |stack|
           if (Time.now - Time.parse(stack[:created_at])).to_i > allowed_age.days
             deleted << stack
-            @gate.delete_stack stack_id: stack[:stack_id]
+            delete_stack_and_associated_resources stack_id: stack[:stack_id]
           end
         end
         deleted
       end
 
+      def delete_stack_and_associated_resources stack_id
+        puts "are their running instances?"
+        while (does_stack_have_apps? stack_id)
+          puts "> delete the apps"
+          delete_all_apps_for_stack stack_id
+          sleep @sleeptime
+        end
+        while (!are_all_instances_stopped_for_stack? stack_id)
+          puts "> stop the instances"
+          stop_all_instances_for_stack stack_id
+          sleep @sleeptime
+        end
+        puts "are there any instances?"
+        while (does_stack_have_instances? stack_id)
+          puts "> delete the instances"
+          delete_all_instances_for_stack stack_id
+          sleep @sleeptime
+        end
+        puts "does the stack have layers?"
+        while(does_stack_have_layers? stack_id)
+          puts "> delete the layers"
+          delete_all_layers_for_stack stack_id
+          sleep @sleeptime
+        end
+        puts "delete the stack"
+        @gate.delete_stack stack_id: stack_id
+      end 
+    
       # does layer have instances?
       def does_layer_have_instances? layer_id
         result = false
         @gate.describe_stacks[:stacks].each do |stack|
           @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
-            @gate.describe_instances(layer_id: layer_id)[:instances].each do |instance|
+            @gate.describe_instances(layer_id: layer[:layer_id])[:instances].each do |instance|
               result = true
               break
             end
@@ -71,12 +101,60 @@ module Cloudpatrol
         return result
       end
 
-      def stop_all_instances_for_layer layer_id
-        result = []
-        @gate.describe_stacks[:stacks].each do |stack|
+      def does_stack_have_instances? stack_id
+        result = false
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
           @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
             @gate.describe_instances(layer_id: layer[:layer_id])[:instances].each do |instance|
-              if (instance[:status] == "running")
+              result = true
+              break
+            end
+          end
+        end
+        return result
+      end
+
+      def does_stack_have_apps? stack_id
+        result = false
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
+          @gate.describe_apps(stack_id: stack[:stack_id])[:apps].each do |app|
+            result = true
+            break
+          end
+        end
+        return result
+      end
+
+      def does_stack_have_apps? stack_id
+        result = false
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
+          @gate.describe_apps(stack_id: stack[:stack_id])[:apps].each do |app|
+            result = true
+            break
+          end
+        end
+        return result
+      end
+
+
+      def delete_all_apps_for_stack stack_id
+        result = []
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
+          @gate.describe_apps(stack_id: stack[:stack_id])[:apps].each do |app|
+            result << app
+            @gate.delete_app app_id: app[:app_id]
+          end
+        end
+        return result
+      end
+
+      def stop_all_instances_for_stack stack_id
+        result = []
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
+          @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
+            @gate.describe_instances(layer_id: layer[:layer_id])[:instances].each do |instance|
+              puts instance[:hostname]
+              if (instance[:status] != "stopped")
                 @gate.stop_instance instance_id: instance[:instance_id]
                 result << instance
               end
@@ -91,7 +169,24 @@ module Cloudpatrol
         @gate.describe_stacks[:stacks].each do |stack|
           @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
             @gate.describe_instances(layer_id: layer[:layer_id])[:instances].each do |instance|
-              if (instance[:status] == "running")
+            puts instance[:hostname]
+              if (instance[:status] != "stopped")
+                result = false
+                break
+              end
+            end
+          end
+        end
+        return result
+      end
+
+      def are_all_instances_stopped_for_stack? stack_id
+        result = true
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
+          @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
+            @gate.describe_instances(layer_id: layer[:layer_id])[:instances].each do |instance|
+            puts instance[:hostname]
+              if (instance[:status] != "stopped")
                 result = false
                 break
               end
@@ -106,6 +201,21 @@ module Cloudpatrol
         @gate.describe_stacks[:stacks].each do |stack|
           @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
             @gate.describe_instances(layer_id: layer[:layer_id])[:instances].each do |instance|
+              puts instance[:hostname]
+              @gate.delete_instance instance_id: instance[:instance_id]
+              result << instance
+            end
+          end
+        end
+        return result
+      end
+
+      def delete_all_instances_for_stack stack_id
+        result = []
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
+          @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
+            @gate.describe_instances(layer_id: layer[:layer_id])[:instances].each do |instance|
+              puts instance[:hostname]
               @gate.delete_instance instance_id: instance[:instance_id]
               result << instance
             end
@@ -116,7 +226,7 @@ module Cloudpatrol
 
       def delete_all_layers_for_stack stack_id
         result = []
-        @gate.describe_stacks[:stacks].each do |stack|
+        @gate.describe_stacks({:stack_ids => [stack_id]})[:stacks].each do |stack|
           @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
             @gate.delete_layer layer_id: layer[:layer_id]
             result << layer
@@ -127,14 +237,25 @@ module Cloudpatrol
 
       def does_stack_have_layers? stack_id
         result = false
-        @gate.describe_stacks[:stacks].each do |stack|
-          @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
+        @gate.describe_stacks[:stacks].each do |stack| 
+          @gate.describe_layers(stack_id: stack_id)[:layers].each do |layer|
             result = true
             break
           end
         end
         return result        
-      end
+      end    
+
+      # unless stack is empty
+      #   for each layer
+      #     for each instance
+      #       stop instance
+      #       wait for instances to stop
+      #       delete each instances
+      #       wait for instances to delete
+      #     delete layer
+      #     wait for layer to delete
+      # delete stack
 
     end
   end
--- spec/cloudpatrol/task/opsworks_spec.rb
+++ spec/cloudpatrol/task/opsworks_spec.rb
@@ -1,5 +1,18 @@
 require 'Cloudpatrol'
 
+# Know what's really hard to find online? A list of all the possible status an opsowrks instance can be in
+# required
+# booting
+# running_setup
+# online
+# setup_failed
+# start_failed
+# terminating
+# terminated
+# stopped
+# connection_lost
+
+
 describe Cloudpatrol::Task::OpsWorks do
   it "should be able to detect if a layer has running instances" do 
     # create a mock OpsWorks client that returns instances, and assert the method found them
@@ -8,44 +21,44 @@ describe Cloudpatrol::Task::OpsWorks do
     ops = Cloudpatrol::Task::OpsWorks.new Hash.new
     ops.instance_variable_set '@gate', client
 
-    expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => [{ :instances => [] } ]})
-    expect(client).to receive(:describe_instances).and_return ({ :instances => [ { :instance_id => 456 }, { :instance_id => 789} ]})
+    expect(client).to receive(:describe_stacks).with(no_args()).and_return ({ :stacks => [{ :stack_id => 123 }]})
+    expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => [{ :layer_id => 654, :instances => [] } ]})
+    expect(client).to receive(:describe_instances).with({ :layer_id => 654 }).and_return ({ :instances => [ { :instance_id => 456 }, { :instance_id => 789} ]})
 
-    actual = ops.does_layer_have_instances? 'layer'
+    actual = ops.does_layer_have_instances? 654
     actual.should be_true
   end
 
-  it "should be able to detect if a layer has no running instances" do 
-    # create a mock OpsWorks client that returns no instances, and assert the method found none
-    client = double(AWS::OpsWorks::Client)
+  # it "should be able to detect if a layer has no running instances" do 
+  #   # create a mock OpsWorks client that returns no instances, and assert the method found none
+  #   client = double(AWS::OpsWorks::Client)
 
-    ops = Cloudpatrol::Task::OpsWorks.new Hash.new
-    ops.instance_variable_set '@gate', client
+  #   ops = Cloudpatrol::Task::OpsWorks.new Hash.new
+  #   ops.instance_variable_set '@gate', client
 
-    expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => [{ :instances => [] } ]})
-    expect(client).to receive(:describe_instances).and_return ({ :instances => []})
+  #   expect(client).to receive(:describe_stacks).with(no_args()).and_return ({ :stacks => [{ :stack_id => 123 }]})
+  #   expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => [{ :instances => [] } ]})
+  #   expect(client).to receive(:describe_instances).with({ :layer_id => 654 }).and_return ({ :instances => []})
 
-    actual = ops.does_layer_have_instances? 'layer'
-    actual.should be_false
-  end
+  #   actual = ops.does_layer_have_instances? 654
+  #   actual.should be_false
+  # end
 
-  it "should be able to stop all instances in a layer" do
-    # create a mock OpsWorks client that returns instances, and assert the method found them
-    client = double(AWS::OpsWorks::Client)
+  # it "should be able to stop all instances in a layer" do
+  #   # create a mock OpsWorks client that returns instances, and assert the method found them
+  #   client = double(AWS::OpsWorks::Client)
 
-    ops = Cloudpatrol::Task::OpsWorks.new Hash.new
-    ops.instance_variable_set '@gate', client
+  #   ops = Cloudpatrol::Task::OpsWorks.new Hash.new
+  #   ops.instance_variable_set '@gate', client
 
-    expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => [{ :instances => [] } ]})
-    expect(client).to receive(:describe_instances).and_return ({ :instances => [ { :instance_id => 456, :status => "running" }, { :instance_id => 789, :status => "stopped"} ]})
-    expect(client).to receive(:stop_instance).with({ :instance_id => 456 })
+  #   expect(client).to receive(:describe_stacks).with(no_args()).and_return ({ :stacks => [{ :stack_id => 123 }]})
+  #   expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => [{ :layer_id => 654, :instances => [] } ]})
+  #   expect(client).to receive(:describe_instances).with({ :layer_id => 654 }).and_return ({ :instances => [ { :instance_id => 456, :status => "online" }, { :instance_id => 789, :status => "stopped"} ]})
+  #   expect(client).to receive(:stop_instance).with({ :instance_id => 456 })
 
-    actual = ops.stop_all_instances_for_layer 'layer'
-    actual.should be_true
-  end 
+  #   actual = ops.stop_all_instances_for_layer 654
+  #   actual.should be_true
+  # end 
 
   it "should be able to determine if all instances in a layer are stopped" do
     # create a mock OpsWorks client that returns instances, and assert the method found them
@@ -55,10 +68,10 @@ describe Cloudpatrol::Task::OpsWorks do
     ops.instance_variable_set '@gate', client
 
     expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => [{ :instances => [] } ]})
-    expect(client).to receive(:describe_instances).and_return ({ :instances => [ { :instance_id => 456, :status => "stopped" }, { :instance_id => 789, :status => "stopped"} ]})
+    expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => [{ :layer_id => 654, :instances => [] } ]})
+    expect(client).to receive(:describe_instances).with({ :layer_id => 654}).and_return ({ :instances => [ { :instance_id => 456, :status => "stopped" }, { :instance_id => 789, :status => "stopped"} ]})
 
-    actual = ops.are_all_instances_stopped_for_layer? 'layer'
+    actual = ops.are_all_instances_stopped_for_layer? 654
     actual.should be_true
   end 
 
@@ -70,10 +83,10 @@ describe Cloudpatrol::Task::OpsWorks do
     ops.instance_variable_set '@gate', client
 
     expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => [{ :instances => [] } ]})
-    expect(client).to receive(:describe_instances).and_return ({ :instances => [ { :instance_id => 456, :status => "running" }, { :instance_id => 789, :status => "stopped"} ]})
+    expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => [{ :layer_id => 654, :instances => [] } ]})
+    expect(client).to receive(:describe_instances).with({ :layer_id => 654 }).and_return ({ :instances => [ { :instance_id => 456, :status => "online" }, { :instance_id => 789, :status => "stopped"} ]})
 
-    actual = ops.are_all_instances_stopped_for_layer? 'layer'
+    actual = ops.are_all_instances_stopped_for_layer? 654
     actual.should be_false
   end 
 
@@ -84,13 +97,13 @@ describe Cloudpatrol::Task::OpsWorks do
     ops = Cloudpatrol::Task::OpsWorks.new Hash.new
     ops.instance_variable_set '@gate', client
 
-    expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => [{ :instances => [] } ]})
-    expect(client).to receive(:describe_instances).and_return ({ :instances => [ { :instance_id => 456, :status => "running" }, { :instance_id => 789, :status => "stopped"} ]})
+    expect(client).to receive(:describe_stacks).with(no_args()).and_return ({ :stacks => [{ :stack_id => 123 }]})
+    expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => [{ :layer_id => 654, :instances => [] } ]})
+    expect(client).to receive(:describe_instances).with({ :layer_id => 654  }).and_return ({ :instances => [ { :instance_id => 456, :status => "online" }, { :instance_id => 789, :status => "shutdown"} ]})
     expect(client).to receive(:delete_instance).with({ :instance_id => 456 } )
     expect(client).to receive(:delete_instance).with({ :instance_id => 789 } )
 
-    actual = ops.delete_all_instances_for_layer 'layer'
+    actual = ops.delete_all_instances_for_layer 654
     actual.should be_true
   end 
 
@@ -102,10 +115,10 @@ describe Cloudpatrol::Task::OpsWorks do
     ops = Cloudpatrol::Task::OpsWorks.new Hash.new
     ops.instance_variable_set '@gate', client
 
-    expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => [{ :instances => [] } ]})
+    expect(client).to receive(:describe_stacks).with(no_args()).and_return ({ :stacks => [{ :stack_id => 123 }]})
+    expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => [{ :instances => [] } ]})
   
-    actual = ops.does_stack_have_layers? 'layer'
+    actual = ops.does_stack_have_layers? 123
     actual.should be_true
   end 
 
@@ -116,11 +129,36 @@ describe Cloudpatrol::Task::OpsWorks do
     ops = Cloudpatrol::Task::OpsWorks.new Hash.new
     ops.instance_variable_set '@gate', client
 
-    expect(client).to receive(:describe_stacks).and_return ({ :stacks => [{ :stack_id => 123 }]})
-    expect(client).to receive(:describe_layers).and_return ({ :layers => []})
+    expect(client).to receive(:describe_stacks).with(no_args()).and_return ({ :stacks => [{ :stack_id => 123 }]})
+    expect(client).to receive(:describe_layers).with({ :stack_id => 123 }).and_return ({ :layers => []})
   
-    actual = ops.does_stack_have_layers? 'layer'
+    actual = ops.does_stack_have_layers? 123
     actual.should be_false
   end 
+
+  # this test does a lot of looping while waiting for state changes. it can be tested, but it's going to require more time than I'm willing to dedicate to it today
+  # it "should be able to delete a stack and all associated resources" do
+  #   # create a mock OpsWorks client that returns instances, and assert the method found them
+  #   client = double(AWS::OpsWorks::Client)
+
+  #   ops = Cloudpatrol::Task::OpsWorks.new Hash.new
+  #   ops.instance_variable_set '@gate', client
+  #   ops.instance_variable_set '@sleeptime', 0
+
+  #   expect(client).to receive(:describe_stacks).exactly(3).times.and_return ({ :stacks => [{ :stack_id => 123 }]})
+  #   expect(client).to receive(:describe_layers).exactly(4).times.and_return ({ :layers => [{ :layer_id => 321, :instances => [] } ]})
+  #   expect(client).to receive(:describe_instances).times.and_return ({ :instances => [ { :instance_id => 456, :status => "online" }, { :instance_id => 789, :status => "shutdown"} ]})
+  #   expect(client).to receive(:stop_instance).with({ :instance_id => 456 })
+  #   expect(client).to receive(:stop_instance).with({ :instance_id => 789 })
+  #   expect(client).to receive(:delete_instance).with({ :instance_id => 456 } )
+  #   expect(client).to receive(:delete_instance).with({ :instance_id => 789 } )
+  #   expect(client).to receive(:delete_layer).with({ :layer_id => 789 } )
+  #   expect(client).to receive(:delete_stack).with({ :stack_id => 123467890 } )
+
+
+  #   actual = ops.delete_stack_and_associated_resources 123467890
+
+  #   actual.should be_true
+  # end 
 end
 
