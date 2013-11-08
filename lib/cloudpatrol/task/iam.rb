1d62a0bf40a2273f49775645cfd625e19e332540
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-11-08 11:57:03 -1000

    hardened clean-users so that if it experiences an error deleting a user, it will continue to delete other users.

--- lib/cloudpatrol/task/iam.rb
+++ lib/cloudpatrol/task/iam.rb
@@ -9,10 +9,15 @@ module Cloudpatrol
 
       def clean_users
         deleted = []
-        @gate.users.each do |user|
+        users = @gate.users
+        users.each do |user|
           unless user.name =~ /^_/ or user.mfa_devices.count > 0
-            deleted << user.inspect
-            user.delete!
+            begin
+              user.delete!
+              deleted << user.inspect
+            rescue AWS::Errors::Base => e
+              puts "Failed to delete #{user.name} because of #{e}"
+            end
           end
         end
         deleted
---
+++ spec/cloudpatrol/task/iam_spec.rb
@@ -0,0 +1,82 @@
+require 'Cloudpatrol'
+
+describe Cloudpatrol::Task::IAM do
+
+  it "should be delete users without MFAs" do
+    client = double(AWS::IAM)
+    user = double(AWS::IAM::User)
+    mfacollection = double(AWS::IAM::MFADeviceCollection)
+
+    iam = Cloudpatrol::Task::IAM.new Hash.new
+    iam.instance_variable_set '@gate', client
+
+    expect(client).to receive(:users).with(no_args()).and_return ([user])
+    expect(user).to receive(:name).with(no_args()).and_return("batman")
+    expect(user).to receive(:mfa_devices).with(no_args()).and_return(mfacollection)
+    expect(user).to receive(:delete!).with(no_args())
+    expect(mfacollection).to receive(:count).and_return(0)
+
+
+    actual = iam.clean_users
+    actual.count.should == 1
+    actual.first.should == user.inspect
+  end
+
+  it "shouldn't delete users that start with _" do
+    client = double(AWS::IAM)
+    user = double(AWS::IAM::User)
+    mfacollection = double(AWS::IAM::MFADeviceCollection)
+
+    iam = Cloudpatrol::Task::IAM.new Hash.new
+    iam.instance_variable_set '@gate', client
+
+    expect(client).to receive(:users).with(no_args()).and_return ([user])
+    expect(user).to receive(:name).and_return("_batman")
+
+    actual = iam.clean_users
+    actual.count.should == 0
+  end
+
+  it "shouldn't delete users with MFAs" do
+    client = double(AWS::IAM)
+    user = double(AWS::IAM::User)
+    mfacollection = double(AWS::IAM::MFADeviceCollection)
+
+    iam = Cloudpatrol::Task::IAM.new Hash.new
+    iam.instance_variable_set '@gate', client
+
+    expect(client).to receive(:users).with(no_args()).and_return ([user])
+    expect(user).to receive(:name).with(no_args()).and_return("batman")
+    expect(user).to receive(:mfa_devices).with(no_args()).and_return(mfacollection)
+    expect(mfacollection).to receive(:count).and_return(1)
+
+
+    actual = iam.clean_users
+    actual.count.should == 0
+  end
+
+  it "should handle AWS exceptions cleanly" do
+    client = double(AWS::IAM)
+    user1 = double(AWS::IAM::User)
+    user2 = double(AWS::IAM::User)
+    mfacollection = double(AWS::IAM::MFADeviceCollection)
+
+    iam = Cloudpatrol::Task::IAM.new Hash.new
+    iam.instance_variable_set '@gate', client
+
+    expect(client).to receive(:users).with(no_args()).and_return ([user1, user2])
+    expect(user1).to receive(:name).exactly(2).times.with(no_args()).and_return("batman1")
+    expect(user1).to receive(:mfa_devices).with(no_args()).and_return(mfacollection)
+    expect(user1).to receive(:delete!).and_raise(AWS::Errors::Base, "Test exception")
+
+    expect(user2).to receive(:name).with(no_args()).and_return("batman2")
+    expect(user2).to receive(:mfa_devices).with(no_args()).and_return(mfacollection)
+    expect(user2).to receive(:delete!).with(no_args())
+    expect(mfacollection).to receive(:count).exactly(2).times.and_return(0)
+
+    actual = iam.clean_users
+    actual.count.should == 1
+    actual.first.should == user2.inspect
+  end
+
+end
