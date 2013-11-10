2a1d83cfe4bd704da7741ce4ed9152c60a91128a
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-11-10 10:13:35 -1000

    made cloudformation more fault tolerant.

---
+++ spec/cloudpatrol/task/cloudformation_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+require 'Cloudpatrol'
+
+describe Cloudpatrol::Task::CloudFormation do
+
+  it "should be remove cloudformation stacks over a certain age" do
+    client = double(AWS::CloudFormation)
+    stack = double(AWS::CloudFormation::Stack)
+
+    cfn = Cloudpatrol::Task::CloudFormation.new Hash.new
+    cfn.instance_variable_set '@gate', client
+
+    expect(client).to receive(:stacks).with(no_args()).and_return([stack])
+    expect(stack).to receive(:creation_time).with(no_args()).and_return(Time.now - 3 * 24 * 60 * 60)
+    expect(stack).to receive(:delete).with(no_args())
+
+    actual_successes, actual_failures = cfn.clean_stacks 2
+
+    actual_successes.count.should == 1
+    actual_successes.first.should == stack.inspect
+
+    actual_failures.count.should == 0
+
+  end
+
+  it "it shouldn't delete stacks before a certain age" do
+    client = double(AWS::CloudFormation)
+    stack = double(AWS::CloudFormation::Stack)
+
+    cfn = Cloudpatrol::Task::CloudFormation.new Hash.new
+    cfn.instance_variable_set '@gate', client
+
+    expect(client).to receive(:stacks).with(no_args()).and_return([stack])
+    expect(stack).to receive(:creation_time).with(no_args()).and_return(Time.now - (3 * 24 * 60 * 60))
+
+    actual_successes, actual_failures = cfn.clean_stacks 4
+
+    actual_successes.count.should == 0
+    actual_failures.count.should == 0
+
+  end
+
+
+  it "should handle AWS exceptions cleanly" do
+    client = double(AWS::CloudFormation)
+    stack = double(AWS::CloudFormation::Stack)
+
+    cfn = Cloudpatrol::Task::CloudFormation.new Hash.new
+    cfn.instance_variable_set '@gate', client
+
+    expect(client).to receive(:stacks).with(no_args()).and_return([stack])
+    expect(stack).to receive(:creation_time).with(no_args()).and_return(Time.now - (3 * 24 * 60 * 60))
+    expect(stack).to receive(:delete).with(no_args()).and_raise(AWS::Errors::Base, "Failed to delete stack")
+
+    actual_successes, actual_failures = cfn.clean_stacks 0
+
+    actual_successes.count.should == 0
+    actual_failures.count.should == 1
+    actual_failures.first.should == stack.inspect
+
+  end
+
+end
