8ed3407e61ea21aa6771d9bd5b3689369285cd74
Eric Kascic <eric.kascic@stelligent.com>
2013-11-20 07:54:55 -0500

    remove any opsworks code that applies only to apps/layers/instance.  stacks are the unit of collection

--- lib/cloudpatrol/task/ops_works.rb
+++ lib/cloudpatrol/task/ops_works.rb
@@ -9,63 +9,6 @@ module Cloudpatrol
         @sleeptime = 10
       end
 
-      def clean_apps(allowed_age, whitelist=nil)
-        deleted = []
-        undeleted = []
-        @gate.describe_stacks[:stacks].each do |stack|
-          @gate.describe_apps(stack_id: stack[:stack_id])[:apps].each do |app|
-            if (Time.now - Time.parse(app[:created_at])).to_i > allowed_age.days
-              begin
-                @gate.delete_app app_id: app[:app_id]
-                deleted << app
-              rescue
-                undeleted << app
-              end
-            end
-          end
-        end
-        return deleted, undeleted
-      end
-
-      def clean_instances(allowed_age, whitelist=nil)
-        deleted = []
-        undeleted = []
-        @gate.describe_stacks[:stacks].each do |stack|
-          @gate.describe_instances(stack_id: stack[:stack_id])[:instances].each do |instance|
-            if (Time.now - Time.parse(instance[:created_at])).to_i > allowed_age.days
-              begin
-                @gate.delete_instance instance_id: instance[:instance_id]
-                deleted << instance
-              rescue
-                undeleted << instance
-              end
-            end
-          end
-        end
-        return deleted, undeleted
-      end
-
-      def clean_layers(allowed_age, whitelist=nil)
-        deleted = []
-        undeleted = []
-        @gate.describe_stacks[:stacks].each do |stack|
-          @gate.describe_layers(stack_id: stack[:stack_id])[:layers].each do |layer|
-            if (Time.now - Time.parse(layer[:created_at])).to_i > allowed_age.days
-              begin
-                @gate.delete_layer layer_id: layer[:layer_id]
-                deleted << layer
-              rescue
-                undeleted << layer
-              end
-            end
-          end
-        end
-        return deleted, undeleted
-      end
-
-
-
-
       def clean_stacks(allowed_age, whitelist=nil)
         deleted = []
         undeleted = []
